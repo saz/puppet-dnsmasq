a6addf80a4a1511c8ca9406a447eebc51dc528dc
Saz <me@saz.sh>
2011-08-01 11:54:22 +0200

    include dnsmasq in dnsmasq::conf

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,4 +1,6 @@
 define dnsmasq::conf($ensure = 'present', $prio = 10, $source = undef, $content = undef) {
+    include dnsmasq
+
     file { "${dnsmasq::params::config_dir}${prio}-${name}":
         ensure  => $ensure,
         owner   => root,
