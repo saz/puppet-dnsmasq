285cebe07edda2ee91bbc298536ed0b4fca48520
Saz <me@saz.sh>
2011-08-01 11:51:19 +0200

    Fix typo in variable name

--- manifests/conf.pp
+++ manifests/conf.pp
@@ -1,5 +1,5 @@
 define dnsmasq::conf($ensure = 'present', $prio = 10, $source = undef, $content = undef) {
-    file { "${dnsmasq::params:config_dir}${prio}-${name}":
+    file { "${dnsmasq::params::config_dir}${prio}-${name}":
         ensure  => $ensure,
         owner   => root,
         group   => root,
