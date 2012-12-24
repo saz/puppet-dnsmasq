c7de4e2b825dd71a79ea9641fc3d05599e7a8c61
Lars Kellogg-Stedman <lars@oddbit.com>
2012-12-23 21:23:06 -0500

    Add support for Arch Linux
    This adds support for Arch Linux (assuming that you are running a version
    of Facter that knows about Arch Linux) and collapses identical entries 
    into a single branch of the case statement.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,13 +1,6 @@
 class dnsmasq::params {
   case $::osfamily {
-    debian: {
-      $package_name = 'dnsmasq'
-      $service_name = 'dnsmasq'
-      $config_file = '/etc/dnsmasq.conf'
-      $resolv_file = '/etc/resolv.conf.dnsmasq'
-      $config_dir = '/etc/dnsmasq.d/'
-    }
-    redhat: {
+    debian, redhat, arch: {
       $package_name = 'dnsmasq'
       $service_name = 'dnsmasq'
       $config_file = '/etc/dnsmasq.conf'
