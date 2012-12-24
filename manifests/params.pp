4e7eb042703909b41ca83d5e40d943ac96ba9e97
Lars Kellogg-Stedman <lars@oddbit.com>
2012-12-23 21:28:45 -0500

    More Arch Linux fixes
    This works around the fact that Facter does not set `$osfamily` 
    correctly on Arch Linux.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,14 @@
 class dnsmasq::params {
-  case $::osfamily {
-    debian, redhat, arch: {
+
+  # Facter under ArchLinux reports $osfamily as "Linux", but it
+  # gets $operatingsystem correct.
+  $_family = $osfamily ? {
+    Linux => $operatingsystem,
+    default => $osfamily,
+  }
+
+  case $_family {
+    Debian, RedHat, Archlinux: {
       $package_name = 'dnsmasq'
       $service_name = 'dnsmasq'
       $config_file = '/etc/dnsmasq.conf'
