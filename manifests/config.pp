38b93da52e320cb2f813277300767c8db8c40534
Federico Fapitalle <federico.fapitalle@gmail.com>
2018-05-15 15:39:43 -0300

    manage DNSMASQ_EXCEPT at service defaults file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,6 @@
-class dnsmasq::config {
+class dnsmasq::config (
+  Array $except_interface = undef,
+) {
   File {
     owner => 'root',
     group => 'root',
@@ -7,6 +9,15 @@ class dnsmasq::config {
   file { $::dnsmasq::params::config_file:
     mode   => '0644',
     source => 'puppet:///modules/dnsmasq/dnsmasq.conf',
+  }
+
+  if is_array($except_interfaces) {
+    $except_value = join($except_interface, ' ')
+    file_line { 'except_interfaces':
+      ensure => present,
+      path   => $::dnsmasq::params::defaults_file,
+      line   => "DNSMASQ_EXCEPT=\"${except_value}\"",
+    }
   }
 
 }
