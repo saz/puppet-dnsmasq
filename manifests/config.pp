2063499d3b742573e74f6d379c2009a0484bebf9
Federico Fapitalle <federico.fapitalle@gmail.com>
2018-05-15 15:39:43 -0300

    manage DNSMASQ_EXCEPT at service defaults file
    use match to replace line if exists
    
    manage IGNORE_RESOLVCONF at service defaults file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,10 @@
-class dnsmasq::config {
+class dnsmasq::config (
+  $except_interface  = undef,
+  $ignore_resolvconf = false,
+) {
+
+  validate_bool($ignore_resolvconf)
+
   File {
     owner => 'root',
     group => 'root',
@@ -7,6 +13,26 @@ class dnsmasq::config {
   file { $::dnsmasq::params::config_file:
     mode   => '0644',
     source => 'puppet:///modules/dnsmasq/dnsmasq.conf',
+  }
+
+  if is_array($except_interface) {
+    $except_value = join($except_interface, ' ')
+    file_line { 'defaults_except_interfaces':
+      ensure             => present,
+      path               => $::dnsmasq::params::defaults_file,
+      line               => "DNSMASQ_EXCEPT=\"${except_value}\"",
+      match              => '^#?DNSMASQ_EXCEPT',
+      append_on_no_match => true,
+    }
+  }
+
+  $ign_resolvconf_value = bool2str($ignore_resolvconf,'yes','no')
+  file_line { 'defaults_ignore_resolvconf':
+    ensure             => present,
+    path               => $::dnsmasq::params::defaults_file,
+    line               => "IGNORE_RESOLVCONF=${ign_resolvconf_value}",
+    match              => '^#?IGNORE_RESOLVCONF',
+    append_on_no_match => true,
   }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,18 +1,20 @@
 class dnsmasq::params {
   case $::osfamily {
     'Debian': {
-      $package_name = 'dnsmasq'
-      $service_name = 'dnsmasq'
-      $config_file = '/etc/dnsmasq.conf'
-      $resolv_file = '/etc/resolv.conf.dnsmasq'
-      $config_dir = '/etc/dnsmasq.d/'
+      $package_name  = 'dnsmasq'
+      $service_name  = 'dnsmasq'
+      $config_file   = '/etc/dnsmasq.conf'
+      $resolv_file   = '/etc/resolv.conf.dnsmasq'
+      $defaults_file = '/etc/default/dnsmasq'
+      $config_dir    = '/etc/dnsmasq.d/'
     }
     'RedHat': {
-      $package_name = 'dnsmasq'
-      $service_name = 'dnsmasq'
-      $config_file = '/etc/dnsmasq.conf'
-      $resolv_file = '/etc/resolv.conf.dnsmasq'
-      $config_dir = '/etc/dnsmasq.d/'
+      $package_name  = 'dnsmasq'
+      $service_name  = 'dnsmasq'
+      $config_file   = '/etc/dnsmasq.conf'
+      $resolv_file   = '/etc/resolv.conf.dnsmasq'
+      $defaults_file = '/etc/sysconfig/dnsmasq'
+      $config_dir    = '/etc/dnsmasq.d/'
     }
     default: {
       case $::operatingsystem {
