d368b33994ae2685b5e2ad4bb31a06ed85cfebcc
Federico Fapitalle <federico.fapitalle@gmail.com>
2018-05-15 16:46:29 -0300

    manage IGNORE_RESOLVCONF at service defaults file

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,6 +1,10 @@
 class dnsmasq::config (
-  $except_interface = undef,
+  $except_interface  = undef,
+  $ignore_resolvconf = false,
 ) {
+
+  validate_bool($ignore_resolvconf)
+
   File {
     owner => 'root',
     group => 'root',
@@ -13,13 +17,22 @@ class dnsmasq::config (
 
   if is_array($except_interface) {
     $except_value = join($except_interface, ' ')
-    file_line { 'except_interfaces':
-      ensure => present,
-      path   => $::dnsmasq::params::defaults_file,
-      line   => "DNSMASQ_EXCEPT=\"${except_value}\"",
-      match  => "^DNSMASQ_EXCEPT",
+    file_line { 'defaults_except_interfaces':
+      ensure             => present,
+      path               => $::dnsmasq::params::defaults_file,
+      line               => "DNSMASQ_EXCEPT=\"${except_value}\"",
+      match              => '^#?DNSMASQ_EXCEPT',
       append_on_no_match => true,
     }
+  }
+
+  $ign_resolvconf_value = bool2str($ignore_resolvconf,'yes','no')
+  file_line { 'defaults_ignore_resolvconf':
+    ensure             => present,
+    path               => $::dnsmasq::params::defaults_file,
+    line               => "IGNORE_RESOLVCONF=${ign_resolvconf_value}",
+    match              => '^#?IGNORE_RESOLVCONF',
+    append_on_no_match => true,
   }
 
 }
