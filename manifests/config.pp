d5a14021e2d4171607eab58a9ab01671062d2069
root <federico.fapitalle@gmail.com>
2018-05-15 19:31:15 +0000

    use match to replace line if exists

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,5 @@
 class dnsmasq::config (
-  Array $except_interface = undef,
+  $except_interface = undef,
 ) {
   File {
     owner => 'root',
@@ -11,12 +11,14 @@ class dnsmasq::config (
     source => 'puppet:///modules/dnsmasq/dnsmasq.conf',
   }
 
-  if is_array($except_interfaces) {
+  if is_array($except_interface) {
     $except_value = join($except_interface, ' ')
     file_line { 'except_interfaces':
       ensure => present,
       path   => $::dnsmasq::params::defaults_file,
       line   => "DNSMASQ_EXCEPT=\"${except_value}\"",
+      match  => "^DNSMASQ_EXCEPT",
+      append_on_no_match => true,
     }
   }
 
